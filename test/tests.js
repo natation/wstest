d43b417c9dbacb49b19994575b02df60258ff45d
Vic <vic@aligncommerce.com>
2016-01-11 13:58:01 -0800

    First wstest app

---
+++ test/tests.js
@@ -0,0 +1,12 @@
+var should = require('should');
+var assert = require('assert');
+var threeSeconds = require('seconds');
+
+describe('Test Framework', function () {
+  it('should have mocha installed and running.', function () {
+    assert.equal(true, true);
+  })
+  it('should have the should library installed and running for fluent testing.', function () {
+    true.should.eql(true);
+  })
+});
